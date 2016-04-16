3ebe2ae5d9e5010c24cf5118ab7639c19b62453a
Lauren Fu <laurenfu@microsoft.com>
2016-04-16 22:03:57 +0800

    try

---
+++ auto.js
@@ -0,0 +1,55 @@
+var employee = [{"id":"2103","name":"林ＸＸ","odr":1},{"id":"1400","name":"許ＸＸ","odr":1},{"id":"2795","name":"曾ＸＸ","odr":1},{"id":"2426","name":"陳ＸＸ","odr":1},{"id":"2427","name":"張ＸＸ","odr":1},{"id":"3962","name":"元ＸＸ","odr":1},{"id":"2416","name":"林ＸＸ","odr":1},{"id":"2789","name":"蔡ＸＸ","odr":1},{"id":"2794","name":"紀ＸＸ","odr":1},{"id":"8159","name":"陳ＸＸ","odr":1},{"id":"2455","name":"黃ＸＸ","odr":1},{"id":"3970","name":"陳ＸＸ","odr":1},{"id":"3981","name":"吳ＸＸ","odr":1},{"id":"2824","name":"施ＸＸ","odr":1},{"id":"2112","name":"紀ＸＸ","odr":1},{"id":"2390","name":"謝ＸＸ","odr":1},{"id":"2705","name":"劉ＸＸ","odr":1},{"id":"2840","name":"何ＸＸ","odr":1},{"id":"3453","name":"陳ＸＸ","odr":1},{"id":"3546","name":"盧ＸＸ","odr":1},{"id":"3801","name":"涂ＸＸ","odr":1},{"id":"3958","name":"游ＸＸ","odr":1},{"id":"3966","name":"吳ＸＸ","odr":1},{"id":"3968","name":"劉ＸＸ","odr":1},{"id":"3969","name":"陳ＸＸ","odr":1},{"id":"3975","name":"劉ＸＸ","odr":1},{"id":"4131","name":"李ＸＸ","odr":1},{"id":"4142","name":"郭ＸＸ","odr":1},{"id":"4151","name":"林ＸＸ","odr":1},{"id":"4418","name":"王ＸＸ","odr":1},{"id":"7489","name":"黃ＸＸ","odr":1},{"id":"3489","name":"蕭ＸＸ","odr":1},{"id":"3605","name":"鐘ＸＸ","odr":1},{"id":"3816","name":"詹ＸＸ","odr":1},{"id":"3897","name":"張ＸＸ","odr":1},{"id":"3963","name":"葉ＸＸ","odr":1},{"id":"3982","name":"吳ＸＸ","odr":1},{"id":"4124","name":"黃ＸＸ","odr":1},{"id":"4133","name":"鄭ＸＸ","odr":1},{"id":"4139","name":"劉ＸＸ","odr":1},{"id":"4140","name":"陳ＸＸ","odr":1},{"id":"4271","name":"張ＸＸ","odr":1},{"id":"4280","name":"江ＸＸ","odr":1},{"id":"4407","name":"賴ＸＸ","odr":1},{"id":"4408","name":"張ＸＸ","odr":1},{"id":"4412","name":"陳ＸＸ","odr":1},{"id":"4417","name":"曾ＸＸ","odr":1},{"id":"4419","name":"游ＸＸ","odr":1},{"id":"4454","name":"李ＸＸ","odr":1},{"id":"4988","name":"鄭ＸＸ","odr":1},{"id":"4989","name":"王ＸＸ","odr":1},{"id":"4990","name":"李ＸＸ","odr":1},{"id":"5012","name":"張ＸＸ","odr":1},{"id":"5015","name":"董ＸＸ","odr":1},{"id":"5328","name":"吳ＸＸ","odr":1},{"id":"5336","name":"楊ＸＸ","odr":1},{"id":"6321","name":"陳ＸＸ","odr":1},{"id":"6603","name":"黃ＸＸ","odr":1},{"id":"7725","name":"吳ＸＸ","odr":1},{"id":"8012","name":"周ＸＸ","odr":1},{"id":"5740","name":"陳ＸＸ","odr":1},{"id":"5741","name":"張ＸＸ","odr":1},{"id":"5760","name":"黃ＸＸ","odr":1},{"id":"5771","name":"陳ＸＸ","odr":1},{"id":"6028","name":"林ＸＸ","odr":1},{"id":"6132","name":"洪ＸＸ","odr":1},{"id":"6140","name":"陳ＸＸ","odr":1},{"id":"6143","name":"施ＸＸ","odr":1},{"id":"6602","name":"吳ＸＸ","odr":1},{"id":"6605","name":"劉ＸＸ","odr":1},{"id":"6964","name":"陳ＸＸ","odr":1},{"id":"6966","name":"詹ＸＸ","odr":1},{"id":"6967","name":"白ＸＸ","odr":1},{"id":"6987","name":"黎ＸＸ","odr":1},{"id":"7114","name":"曾ＸＸ","odr":1},{"id":"7347","name":"虞ＸＸ","odr":1},{"id":"7369","name":"林ＸＸ","odr":1},{"id":"7384","name":"朱ＸＸ","odr":1},{"id":"7954","name":"梁ＸＸ","odr":1},{"id":"8011","name":"李ＸＸ","odr":1},{"id":"8150","name":"林ＸＸ","odr":1},{"id":"5738","name":"林ＸＸ","odr":1},{"id":"6965","name":"蔡ＸＸ","odr":1},{"id":"6986","name":"陳ＸＸ","odr":1},{"id":"7084","name":"陳ＸＸ","odr":1},{"id":"7107","name":"李ＸＸ","odr":1},{"id":"7349","name":"張ＸＸ","odr":1},{"id":"7350","name":"黃ＸＸ","odr":1},{"id":"8013","name":"張ＸＸ","odr":1},{"id":"8099","name":"林ＸＸ","odr":1},{"id":"5196","name":"胡ＸＸ","odr":1},{"id":"6240","name":"廖ＸＸ","odr":1},{"id":"6292","name":"黃ＸＸ","odr":1},{"id":"6614","name":"崔ＸＸ","odr":1},{"id":"6968","name":"陳ＸＸ","odr":1},{"id":"6969","name":"杜ＸＸ","odr":1},{"id":"7115","name":"李ＸＸ","odr":1},{"id":"7457","name":"張ＸＸ","odr":1},{"id":"7568","name":"林ＸＸ","odr":1},{"id":"8043","name":"吳ＸＸ","odr":1},{"id":"8068","name":"陳ＸＸ","odr":1},{"id":"8090","name":"王ＸＸ","odr":1},{"id":"8140","name":"劉ＸＸ","odr":1},{"id":"8201","name":"","odr":1},{"id":"8205","name":"","odr":1},{"id":"8209","name":"","odr":1},{"id":"5449","name":"劉ＸＸ","odr":1},{"id":"7461","name":"陳ＸＸ","odr":1},{"id":"7598","name":"林ＸＸ","odr":1},{"id":"7698","name":"陳ＸＸ","odr":1},{"id":"8184","name":"許ＸＸ","odr":1},{"id":"8185","name":"陳ＸＸ","odr":1},{"id":"8186","name":"黃ＸＸ","odr":1},{"id":"8187","name":"林ＸＸ","odr":1},{"id":"8188","name":"翁ＸＸ","odr":1},{"id":"8189","name":"許ＸＸ","odr":1}];
+
+function sortedCompleteList(arr){
+  return arr.reduce(function(prev,curv){
+    return prev.concat({"id":curv.id,"name":curv.name});
+  },[]).sort(function(a,b){
+    if( parseInt(a.id) < parseInt(b.id)){
+      return -1;
+    }
+    else if( parseInt(a.id) > parseInt(b.id)){
+      return 1;
+    }
+    return 0;
+  }) 
+}
+
+function makeOptionList(arr,string){
+  var matchCount=0;
+  var OptionList =[];
+  if(string.length>0){
+    arr.map(function(obj){
+      if(obj.id.startsWith(string)){
+        console.log(obj);
+        matchCount++;
+        OptionList.push(obj); 
+      }
+    });
+  }
+  return OptionList;
+}
+
+
+var app = angular.module("app",[]);
+app.controller("ctrl",function($scope){
+  $scope.idList = sortedCompleteList(employee);
+
+  $scope.listenChange = function(){
+    document.getElementById('completeList').style.display = 'block';
+    $scope.selectList = makeOptionList($scope.idList,$scope.search);
+  };
+  
+  $scope.hideList = function(){
+    document.getElementById('completeList').style.display = 'none';
+  };
+  
+  $scope.setInputValue = function(Id){
+    console.log("yo");
+    $scope.search = Id;
+  };
+  
+  $scope.showList = function(){
+    document.getElementById('completeList').style.display = 'block';
+  };
+});
+
